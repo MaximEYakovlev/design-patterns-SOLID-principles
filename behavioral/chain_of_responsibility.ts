fd5238973af64bb9941dc5d7826a5d4fa4d6b19a
Maxim Yakovlev <it.trainee@yandex.ru>
2024-08-01 10:09:47 +0300

    added: client code, refactoring

--- behavioral/chain_of_responsibility.ts
+++ behavioral/chain_of_responsibility.ts
@@ -1,27 +1,26 @@
-interface Handler<Request = string, Result = string | null> {
+interface Handler<Request = string, Result = string> {
     setNext(handler: Handler<Request, Result>): Handler<Request, Result>;
     handle(request: Request): Result;
 }
 
-abstract class AbstractHandler implements Handler<string, string | null> {
-    private nextHandler: Handler<string, string | null> | null = null;
+abstract class AbstractHandler implements Handler<string, string> {
+    private nextHandler: Handler<string, string> | null = null;
 
-    public setNext(handler: Handler<string, string | null>): Handler<string, string | null> {
+    public setNext(handler: Handler<string, string>): Handler<string, string> {
         this.nextHandler = handler;
         return handler;
     }
 
-    public handle(request: string): string | null {
+    public handle(request: string): string {
         if (this.nextHandler) {
             return this.nextHandler.handle(request);
         }
-
-        return null;
+        return `the HTTP ${request} method was not recognized`;
     }
 }
 
 class GetHandler extends AbstractHandler {
-    public handle(request: string): string | null {
+    public handle(request: string): string {
         if (request === 'GET') {
             return `the HTTP ${request} method is called`;
         }
@@ -30,7 +29,7 @@ class GetHandler extends AbstractHandler {
 }
 
 class PostHandler extends AbstractHandler {
-    public handle(request: string): string | null {
+    public handle(request: string): string {
         if (request === 'POST') {
             return `the HTTP ${request} method is called`;
         }
@@ -39,10 +38,32 @@ class PostHandler extends AbstractHandler {
 }
 
 class PutHandler extends AbstractHandler {
-    public handle(request: string): string | null {
+    public handle(request: string): string {
         if (request === 'PUT') {
             return `the HTTP ${request} method is called`;
         }
         return super.handle(request);
     }
 }
+
+// client code
+const clientCode = (handler: Handler): void => {
+    const methods = ['GET', 'POST', 'PUT'];
+
+    methods.forEach(method => {
+        const result = handler.handle(method);
+
+        if (result) {
+            console.log(result);
+        }
+    })
+}
+
+const getHandler = new GetHandler();
+const postHandler = new PostHandler();
+const putHandler = new PutHandler();
+
+getHandler.setNext(postHandler).setNext(putHandler);
+
+clientCode(getHandler);
+clientCode(postHandler);
