e4932ecceecf802d14f7de45069dae1b2597e6bd
Maxim Yakovlev <it.trainee@yandex.ru>
2024-08-04 11:45:39 +0300

    added: IFlyweightFactory interface, specific FlyweightFactory class implementation

--- structural/flyweight.ts
+++ structural/flyweight.ts
@@ -2,6 +2,10 @@ interface IFlyweight {
     operation(uniqueState: string[]): void;
 }
 
+interface IFlyweightFactory {
+    getFlyweight(sharedState: string[]): IFlyweight;
+}
+
 class Flyweight implements IFlyweight {
     private sharedState: string[];
 
@@ -13,5 +17,28 @@ class Flyweight implements IFlyweight {
         const sState = JSON.stringify(this.sharedState);
         const uState = JSON.stringify(uniqueState);
         console.log(`Flyweight: shared (${sState}) and unique (${uState}) state`);
+    }
+}
+
+class FlyweightFactory implements IFlyweightFactory {
+    private flyweights: { [key: string]: IFlyweight } = {};
+
+    constructor(initialFlyweights: string[][]) {
+        initialFlyweights.forEach(state => {
+            const key = this.getKey(state);
+            this.flyweights[key] = new Flyweight(state);
+        })
+    }
+
+    private getKey(state: string[]): string {
+        return state.join('_');
+    }
+
+    public getFlyweight(sharedState: string[]): IFlyweight {
+        const key = this.getKey(sharedState);
+
+        // some logic
+
+        return this.flyweights[key];
     }
 }
