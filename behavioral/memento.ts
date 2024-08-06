04e503a628836779047c107d5a1707d966d7dad6
Maxim Yakovlev <it.trainee@yandex.ru>
2024-08-06 12:39:48 +0300

    memento: classes Originator, Memento are implemented

---
+++ behavioral/memento.ts
@@ -0,0 +1,67 @@
+interface IOriginator {
+    save(): IMemento;
+    restore(memento: IMemento): void;
+    doSomeJob(): void;
+}
+
+interface IMemento {
+    getState(): string;
+    getName(): string;
+    getDate(): string;
+}
+
+class Originator implements IOriginator {
+    private state: string;
+
+    constructor(state: string) {
+        this.state = state;
+        console.log(`Originator: initial state: ${state}`);
+    }
+
+    public doSomeJob(): void {
+        console.log('Originator: some kind of job is being done');
+        this.state = this.generateRandomString(26);
+        console.log(`Originator: the state has changed to: ${this.state}`);
+    }
+
+    private generateRandomString(length: number = 10): string {
+        const charSet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        return Array.from({ length },
+            () => charSet.charAt(Math.floor(Math.random() * charSet.length)))
+            .join('');
+    }
+
+    public save(): IMemento {
+        return new Memento(this.state);
+    }
+
+    public restore(memento: IMemento): void {
+        this.state = memento.getState();
+        console.log(`Originator: the state has changed to: ${this.state}`);
+    }
+}
+
+class Memento implements IMemento {
+    private state: string;
+    private date: string;
+
+    constructor(state: string) {
+        this.state = state;
+        this.date = new Date().toISOString().slice(0, 19).replace('T', ' ');
+    }
+
+    public getState(): string {
+        return this.state;
+    }
+
+    public getName(): string {
+        return `${this.date} / (${this.state.substr(0, 9)}...)`;
+    }
+
+    public getDate(): string {
+        return this.date;
+    }
+}
+
+// to do
+class Caretaker { }
