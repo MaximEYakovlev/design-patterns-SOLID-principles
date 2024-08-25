00a665b54b9c3ab9f593abf5f5b5c6e7927dd548
Maxim Yakovlev <it.trainee@yandex.ru>
2024-08-25 21:20:56 +0300

    added dependency injection types

--- solid/dip/dip.js
+++ solid/dip/dip.js
@@ -16,6 +16,8 @@ class LocalStorage {
     }
 }
 
+// method parameters injection
+// ***************************
 class Repository {
     save(data, storage) {
         storage.save(data);
@@ -27,3 +29,22 @@ const fileSystem = new FileSystem();
 const repository = new Repository();
 
 repository.save('some data', fileSystem);
+
+// constructor injection
+// *********************
+class Repository {
+    constructor(storage) {
+        this.storage = storage;
+    }
+
+    save(data) {
+        this.storage.save(data);
+    }
+}
+
+// use case
+const fileSystem = new FileSystem();
+const repository = new Repository(fileSystem);
+
+repository.save('some data');
+
